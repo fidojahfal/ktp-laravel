24b6493acf0189a537b16d7a4071cf197f635bdc
fidojahfal <fidojahfalprayoga@gmail.com>
2024-08-05 13:56:09 +0700

    Add all route to handle route

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,25 @@ Route::get('/login', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+
+Route::get('/login', 'App\Http\Controllers\UserController@viewLogin')->name('user.viewLogin');
+Route::post('/login', 'App\Http\Controllers\UserController@login')->name('user.login');
+Route::get('/register', 'App\Http\Controllers\UserController@viewRegister')->name('user.viewRegister');
+Route::post('/register', 'App\Http\Controllers\UserController@register')->name('user.register');
+Route::get("/logout", "App\Http\Controllers\UserController@logout")->name("user.logout");
+Route::get('/admin/user', 'App\Http\Controllers\UserController@index')->name('user');
+Route::delete('/admin/user/delete/{id}', 'App\Http\Controllers\UserController@delete')->name('user.delete');
+Route::get('/admin/edit/{id}', 'App\Http\Controllers\UserController@viewEdit')->name('user.viewEdit');
+Route::put('/admin/edit/{id}', 'App\Http\Controllers\UserController@edit')->name('user.edit');
+Route::get('/user', 'App\Http\Controllers\UserController@getUser')->name('user.getUser');
+
+Route::get('/admin/ktp', 'App\Http\Controllers\Data_KtpController@index')->name('data_ktp.index');
+Route::delete('/admin/ktp/delete/{id}', 'App\Http\Controllers\Data_KtpController@delete');
+Route::get('/ktp', 'App\Http\Controllers\Data_KtpController@getKtp')->name('data_ktp.getKtp');
+
+Route::get('/ktp/pdf/download', 'App\Http\Controllers\FileController@downloadPdfKtp')->name('data_ktp.download');
+Route::get('/user/pdf/download', 'App\Http\Controllers\FileController@downloadPdfUser')->name('user.download');
+Route::get('/ktp/excel/download', 'App\Http\Controllers\FileController@exportKtp')->name('data_ktp.export');
+Route::get('/user/excel/download', 'App\Http\Controllers\FileController@exportUser')->name('user.export');
+
+Route::get('/send/{email}', 'App\Http\Controllers\MailController@send')->name('email');
