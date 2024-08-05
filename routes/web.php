5821feb80f1137597f5bfd4b7c4fec3e383a93bc
fidojahfal <fidojahfalprayoga@gmail.com>
2024-08-05 14:28:06 +0700

    Add redirect to login in base route

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/login', function () {
-    return "Login";
+    return redirect('login');
 });
 
 Route::get('/dashboard', function () {
